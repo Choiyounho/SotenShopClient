c7fedf30c42faa48b8f887fc518a4d6e13a8769e
Choiyounho <maxosa78@gmail.com>
2021-09-23 18:03:00 +0900

    [docs] : 라이브러리 추가

--- app/src/main/java/com/soten/sotenshopclient/ui/MainActivity.kt
+++ app/src/main/java/com/soten/sotenshopclient/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.soten.sotenshopclient
+package com.soten.sotenshopclient.ui
 
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -7,6 +7,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.soten.sotenshopclient.R
 import com.soten.sotenshopclient.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -22,8 +23,6 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
             R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
         setupActionBarWithNavController(navController, appBarConfiguration)
