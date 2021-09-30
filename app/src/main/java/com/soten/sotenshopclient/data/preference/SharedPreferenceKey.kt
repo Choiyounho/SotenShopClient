b3b46f1603bf7997d30b0b4a114aeb28ba910bd6
Choiyounho <maxosa78@gmail.com>
2021-10-01 04:10:39 +0900

    [feat] : SharedPreference

---
+++ app/src/main/java/com/soten/sotenshopclient/data/preference/SharedPreferenceKey.kt
@@ -0,0 +1,12 @@
+package com.soten.sotenshopclient.data.preference
+
+object SharedPreferenceKey {
+
+    const val KEY_TOKEN = "KEY_TOKEN"
+    const val KEY_REFRESH_TOKEN = "KEY_REFRESH_TOKEN"
+    const val KEY_USER_NAME = "KEY_USER_NAME"
+    const val KEY_USER_ID = "KEY_USER_ID"
+    const val KEY_USER_EMAIL = "KEY_USER_EMAIL"
+    const val KEY_USER_PASSWORD = "KEY_USER_PASSWORD"
+
+}
--- app/src/main/java/com/soten/sotenshopclient/data/preference/SharedPreferenceManager.kt
+++ app/src/main/java/com/soten/sotenshopclient/data/preference/SharedPreferenceManager.kt
@@ -2,6 +2,14 @@ package com.soten.sotenshopclient.data.preference
 
 import android.content.SharedPreferences
 import androidx.core.content.edit
+import com.soten.sotenshopclient.data.preference.SharedPreferenceKey.KEY_REFRESH_TOKEN
+import com.soten.sotenshopclient.data.preference.SharedPreferenceKey.KEY_TOKEN
+import com.soten.sotenshopclient.data.preference.SharedPreferenceKey.KEY_USER_EMAIL
+import com.soten.sotenshopclient.data.preference.SharedPreferenceKey.KEY_USER_ID
+import com.soten.sotenshopclient.data.preference.SharedPreferenceKey.KEY_USER_NAME
+import com.soten.sotenshopclient.data.preference.SharedPreferenceKey.KEY_USER_PASSWORD
+import com.soten.sotenshopclient.data.request.auth.SignInRequest
+import com.soten.sotenshopclient.data.response.auth.SignInResponse
 import javax.inject.Inject
 
 class SharedPreferenceManager @Inject constructor(
@@ -11,13 +19,31 @@ class SharedPreferenceManager @Inject constructor(
     fun getString(key: String): String? =
         sharedPreferences.getString(key, null)
 
-    fun putString(key: String, value: String) =
+    fun putString(key: String, value: String?) =
         sharedPreferences.edit { putString(key, value) }
 
-    fun getLong(key: String): Long? =
-        sharedPreferences.getLong(key, 0)
+    fun getInt(key: String): Int =
+        sharedPreferences.getInt(key, Int.MIN_VALUE)
 
-    fun putLong(key: String, value: Long) =
-        sharedPreferences.edit { putLong(key, value) }
+    fun putInt(key: String, value: Int) =
+        sharedPreferences.edit { putInt(key, value) }
+
+    fun saveUserInfo(response: SignInResponse, signInRequest: SignInRequest) {
+        putString(KEY_TOKEN, response.token)
+        putString(KEY_REFRESH_TOKEN, response.refreshToken)
+        putString(KEY_USER_NAME, response.userName)
+        putInt(KEY_USER_ID, response.userId)
+        putString(KEY_USER_EMAIL, signInRequest.email!!)
+        putString(KEY_USER_PASSWORD, signInRequest.password!!)
+    }
+
+    fun resetUserInfo() {
+        putString(KEY_TOKEN, null)
+        putString(KEY_REFRESH_TOKEN, null)
+        putString(KEY_USER_NAME, null)
+        putInt(KEY_USER_ID, Int.MIN_VALUE)
+        putString(KEY_USER_EMAIL, null)
+        putString(KEY_USER_PASSWORD, null)
+    }
 
 }
